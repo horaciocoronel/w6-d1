a9876cd37cb363344ca4592b0bfb35f1decab0c9
horaciocoronel <arielc@gmail.com>
2017-10-16 12:19:18 -0400

    Finish Reinforcing exercise

--- 01-reinforcing.rb
+++ 01-reinforcing.rb
@@ -1,9 +1,9 @@
-# Write code to generate a hash where the keys are the numbers from 1 to 50 and the values follow these rules:
+#  Write code to generate a hash where the keys are the numbers from 1 to 50 and the values follow these rules:
 #
 # if the number is divisible by 2 the value should be one more than the key
 # if the number is divisible by 7 the value should be one less than the key
 # if the number is divisible by 2 and 7 the value should be the key multiplied by 2
-# otherwise the value should be the same number as the key
+# otherwise the value should be the same number as the  key
 
 def my_method
   hash = Hash.new
